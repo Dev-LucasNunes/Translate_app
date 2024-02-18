744c7d7d21317eff3533c6469e4508ff149666f5
Dev-LucasNunes <dev.lucasnunes@gmail.com>
2024-02-18 17:21:46 -0300

    implementando gems Tty-prompt, tty-progressbar

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 
 gem "debug", require: false
+gem "tty-prompt"
+gem 'tty-progressbar'
--- app.rb
+++ app.rb
@@ -1,12 +1,27 @@
-require_relative 'translate'
-require_relative  'write'
+# require_relative 'translate'
+# require_relative  'write'
+require 'tty-prompt'
+require 'tty-progressbar'
 
-source_text = "Hello World!"
-source_lang = "en"
-target_lang = "pt-BR"
+# puts "Escreva um texto"
+# source_text = gets.chomp
+# puts "Digite a lingua de origem"
+# source_lang = gets.chomp
+# puts "Digite a lingua da tradução"
+# target_lang = gets.chomp
 
-translate = Translate.new
-translated_text = translate.translate_text(source_text, source_lang, target_lang)
+# translate = Translate.new
+# translated_text = translate.translate_text(source_text, source_lang, target_lang)
 
-file = Text.new
-file.translate_file(source_text, translated_text)
+# file = Text.new
+# file.translate_file(source_text, translated_text)
+
+prompt = TTY::Prompt.new
+# response = prompt.select('Escolha uma cor:', %w(Red Green Blue))
+
+# puts "Você escolheu: #{response}"
+
+def progress_bar
+bar = TTY::ProgressBar.new("Progresso [:bar]", total: 30)
+30.times { sleep(0.1); bar.advance }
+end
